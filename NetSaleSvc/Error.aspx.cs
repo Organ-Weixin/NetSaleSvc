bc0927f02db7cf379f748ccf62adf6be5ed3bd42
庆成 <@AUGUST>
2016-08-28 10:14:55 +0800

    错误提示

---
+++ NetSaleSvc/Error.aspx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetSaleSvc
+{
+    public partial class Error : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("请按要求传递正确路径及参数，非常感谢！");
+        }
+    }
+}
